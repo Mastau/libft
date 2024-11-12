81d337b2ef9ce69630ffa17a194b9c8500e38f26
Thomas Arnaud <thomarna@student.42angouleme.fr>
2024-11-12 18:45:23 +0100

    Norm Clear_str

--- str/clear_str.c
+++ str/clear_str.c
@@ -6,7 +6,7 @@
 /*   By: thomarna <thomarna@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 18:22:09 by thomarna          #+#    #+#             */
-/*   Updated: 2024/11/07 18:24:17 by thomarna         ###   ########.fr       */
+/*   Updated: 2024/11/12 18:44:17 by thomarna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	clear_str(char **str)
 
 	cpy = str;
 	if (!str)
-		return;
-	while(*str)
+		return ;
+	while (*str)
 	{
 		free(*str);
 		str++;
